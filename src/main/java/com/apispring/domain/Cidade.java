1510578fc592e8c70ea0f4f2e9fd98587b35aea9
johnviana <johnvianaluz@hotmail.com>
2023-01-18 15:53:54 -0300

    feat: criando os campo da columna cidade

--- src/main/java/com/apispring/domain/Cidade.java
+++ src/main/java/com/apispring/domain/Cidade.java
@@ -1,5 +1,25 @@
 package com.apispring.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Cidade {
+	
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idCidade;
+	
+	@Column
+	private String nome;
 
 }
