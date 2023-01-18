039300d63883439c06755c23db6c1f953e159a8e
johnviana <johnvianaluz@hotmail.com>
2023-01-18 19:55:08 -0300

    feat: criando mapeamento restaurante e cozinha

--- src/main/java/com/apispring/domain/Cozinha.java
+++ src/main/java/com/apispring/domain/Cozinha.java
@@ -1,10 +1,14 @@
 package com.apispring.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -22,5 +26,8 @@ public class Cozinha {
 	
 	@Column
 	private String nome;
+	
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurante = new ArrayList<>();
 
 }
--- src/main/java/com/apispring/domain/Restaurante.java
+++ src/main/java/com/apispring/domain/Restaurante.java
@@ -1,5 +1,31 @@
 package com.apispring.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true )
 public class Restaurante {
 
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idRestaurante;
+	
+	@Column
+	private String nome;
+	
+	@ManyToOne
+	@JoinColumn(name = "cozinha_id")
+	private Cozinha cozinha;
+	
 }
