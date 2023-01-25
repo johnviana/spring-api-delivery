f645130ad56f37db03a6dc99401d82f0a905e86d
johnviana <johnvianaluz@hotmail.com>
2023-01-25 13:44:32 -0300

    criando atributos tabela permissao

--- src/main/java/com/apispring/domain/Permissao.java
+++ src/main/java/com/apispring/domain/Permissao.java
@@ -1,5 +1,30 @@
 package com.apispring.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table
 public class Permissao {
 
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	@Column
+	private Long idPermissao;
+	
+	@Column
+	private String nome;
+	
+	@Column
+	private String descricao;
 }
+
+
