8aef7059241e9dc8b70844b6c52da797afbf41b3
johnviana <johnvianaluz@hotmail.com>
2023-01-25 13:39:41 -0300

    feat: criando atributos na tabela forma de pagamento

--- src/main/java/com/apispring/domain/FormaDePagamento.java
+++ src/main/java/com/apispring/domain/FormaDePagamento.java
@@ -1,5 +1,26 @@
 package com.apispring.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table
+@Entity
 public class FormaDePagamento {
+	
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	@Column
+	private Long idFormaPagamento;
+	
+	@Column(nullable = false)
+	private String descrica;
 
 }
