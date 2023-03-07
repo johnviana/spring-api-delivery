eacdce681f7b9eaadd0967f8b20802e36f7f983c
johnviana <johnvianaluz@hotmail.com>
2023-03-07 14:50:16 -0300

    feat: criando metodo buscar, listar, adicionar

--- src/main/java/com/apiDelivery/api/api/assembler/GrupoInput.java
+++ src/main/java/com/apiDelivery/api/api/assembler/GrupoInput.java
@@ -1,5 +1,15 @@
 package com.apiDelivery.api.api.assembler;
 
+import javax.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class GrupoInput {
 
+    @NotBlank
+    private String nome;
+    
 }
--- src/main/java/com/apiDelivery/api/api/controller/GrupoController.java
+++ src/main/java/com/apiDelivery/api/api/controller/GrupoController.java
@@ -1,9 +1,22 @@
 package com.apiDelivery.api.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.apiDelivery.api.api.Model.GrupoModel;
+import com.apiDelivery.api.api.assembler.GrupoInput;
+import com.apiDelivery.api.api.assembler.GrupoModelAssembler;
+import com.apiDelivery.api.api.assembler.GrupoModelDisassembler;
+import com.apiDelivery.api.domain.model.Grupo;
 import com.apiDelivery.api.domain.service.GrupoService;
 
 @RestController
@@ -13,4 +26,34 @@ public class GrupoController {
 	@Autowired
 	private GrupoService grupoService;
 	
+	@Autowired
+	private GrupoModelAssembler grupoAssembler;
+	
+	@Autowired
+	private GrupoModelDisassembler grupoDisassembler;
+	
+	
+	@GetMapping
+	public List<GrupoModel> listar(){
+		List<Grupo> todosGrupos = grupoService.listarTodos();
+		return grupoAssembler.toCollectionModel(todosGrupos);
+
+	}
+	
+	@GetMapping("/{id}")
+	public GrupoModel buscarGrupo(@PathVariable Long id) {
+		
+		Grupo grupoId = grupoService.buscarOuFalhar(id);
+		return grupoAssembler.toModel(grupoId);
+		
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public GrupoModel salvarGrupo(@RequestBody GrupoInput grupoInput) {
+		Grupo grupo = grupoDisassembler.toDomainObject(grupoInput); 
+		grupo = grupoService.salvar(grupo);
+		
+		return  grupoAssembler.toModel(grupo);
+	}
 }
