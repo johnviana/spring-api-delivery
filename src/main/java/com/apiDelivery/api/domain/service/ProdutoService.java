7ea234f09ae6ce6ee03da8633663027cd2890966
johnviana <johnvianaluz@hotmail.com>
2023-03-09 17:02:51 -0300

    feat: criando produto service

---
+++ src/main/java/com/apiDelivery/api/domain/service/ProdutoService.java
@@ -0,0 +1,33 @@
+package com.apiDelivery.api.domain.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.apiDelivery.api.domain.exception.EntidadeNaoEncontradaExcepetion;
+import com.apiDelivery.api.domain.model.Produto;
+import com.apiDelivery.api.domain.repository.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@Transactional
+	public Produto salvar(Produto produto) {
+		return produtoRepository.save(produto);
+		
+	}
+	
+	public Produto buscarOuFalhar(Long restauranteId, Long produtoId) {
+		return produtoRepository.findbyId(restauranteId, produtoId)
+				.orElseThrow(() -> new EntidadeNaoEncontradaExcepetion(
+						String.format("Entidade não encontrada", restauranteId, produtoId)));
+				
+				
+	}
+
+}
