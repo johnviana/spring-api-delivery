dbdd8321190db401d20083e0a9efe3feedd35462
johnviana <johnvianaluz@hotmail.com>
2023-11-16 15:27:08 -0300

    criando dependencia e configuracao docker

--- src/main/java/com/apiDelivery/api/domain/model/Grupo.java
+++ src/main/java/com/apiDelivery/api/domain/model/Grupo.java
@@ -1,23 +1,20 @@
 package com.apiDelivery.api.domain.model;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
 public class Grupo {
+
+
 
 	@EqualsAndHashCode.Include
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
