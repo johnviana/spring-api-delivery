fdd0ae9a64f2630f6b2cb2ea2c131e45f9c889dc
johnviana <johnvianaluz@hotmail.com>
2023-11-16 15:23:37 -0300

    criando VO para tokenVO

---
+++ src/main/java/com/apiDelivery/api/core/security/TokenVO.java
@@ -0,0 +1,94 @@
+package com.apiDelivery.api.core.security;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class TokenVO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String userName;
+    private Boolean autenticated;
+    private Date created;
+    private Date expiration;
+    private String accessToken;
+    private String refreshToken;
+
+    public TokenVO(){}
+
+    public TokenVO(String userName,
+                   Boolean autenticated,
+                   Date created,
+                   Date expiration,
+                   String accessToken,
+                   String refreshToken) {
+        this.userName = userName;
+        this.autenticated = autenticated;
+        this.created = created;
+        this.expiration = expiration;
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Boolean getAutenticated() {
+        return autenticated;
+    }
+
+    public void setAutenticated(Boolean autenticated) {
+        this.autenticated = autenticated;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(Date expiration) {
+        this.expiration = expiration;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TokenVO tokenVO = (TokenVO) o;
+        return Objects.equals(userName, tokenVO.userName) && Objects.equals(autenticated, tokenVO.autenticated) && Objects.equals(created, tokenVO.created) && Objects.equals(expiration, tokenVO.expiration) && Objects.equals(accessToken, tokenVO.accessToken) && Objects.equals(refreshToken, tokenVO.refreshToken);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userName, autenticated, created, expiration, accessToken, refreshToken);
+    }
+}
